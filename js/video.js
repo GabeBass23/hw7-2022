23d196e7c856bb298579b64bdfc81dff9fa26f30
GabeBass23 <122408427+GabeBass23@users.noreply.github.com>
2023-03-30 17:16:46 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -6,7 +6,9 @@ window.addEventListener("load", function() {
 	video.autoplay = false;
 	video.loop = false;
 	
-	console.log("Good job opening the window")
+	console.log("Good job opening the window");
+	console.log("Auto play is set to false");
+	console.log("Loop is set to false");
 });
 
 document.querySelector("#play").addEventListener("click", function() {
@@ -24,16 +26,28 @@ document.querySelector("#slider").addEventListener("mouseup", function() {
 	volume = document.querySelector("#slider");
 	volumeVal = volume.value/100;
 	video.volume = volumeVal;
+	document.querySelector("#volume").innerText = volume.value + "%"
+	console.log("Current value is ", volume.value/100);
+	console.log(document.querySelector("#volume"));
+});
+
+document.querySelector("#slider").addEventListener("click", function() {
+	volume = document.querySelector("#slider");
+	volumeVal = volume.value/100;
+	video.volume = volumeVal;
+	console.log("Current value is ", volume.value/100);
 });
 
 document.querySelector("#faster").addEventListener("click", function() {
 	video.playbackRate *= 10/9;
-	console.log(video.playbackRate * 100 + "%");
+	console.log("Speed up video")
+	console.log("Speed is ", video.playbackRate);
 });
 
 document.querySelector("#slower").addEventListener("click", function() {
 	video.playbackRate *= 0.9;
-	console.log(video.playbackRate * 100 + "%");
+	console.log("Slow down video")
+	console.log("Speed is ", video.playbackRate);
 });
 
 document.querySelector("#skip").addEventListener("click", function() {
@@ -44,17 +58,20 @@ document.querySelector("#skip").addEventListener("click", function() {
 	else{
 		video.currentTime = 0;
 	}
-	console.log(video.currentTime);
+	console.log("Skip ahead");
+	console.log("Video current time is ", video.currentTime);
 });
 
 document.querySelector("#mute").addEventListener("click", function() {
 	if(video.muted){
 		document.querySelector("#mute").innerText = "Mute";
 		video.muted = false;
+		console.log("Unmute");
 	}
 	else{
-		document.querySelector("#mute").innerText = "Muted";
+		document.querySelector("#mute").innerText = "Unmute";
 		video.muted = true;
+		console.log("Mute");
 	}
 	
 });
